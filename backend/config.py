fb8e5f212b0be30743967ce3aadf42ffa9378358
Paddy <Paddy@test.com>
2024-07-22 15:58:06 +0000

    testing

--- backend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://user:password@db:5432/flask_db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    BASE_URL = 'https://api.nzb.su/api'
-    MARKER = 'XXX'  # Set the marker variable here
